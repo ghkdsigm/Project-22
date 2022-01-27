26681df068de723cb4c46e634ee06701479631d1
2022project <ghkdsigm3@gmail.com>
2022-01-27 10:53:19 +0900

    프로젝트1 두번째 커밋

---
+++ project01/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+
+  <div id="app">
+    <TodoHeader></TodoHeader>
+    <TodoInput></TodoInput>
+    <TodoList></TodoList>
+    <TodoFooter></TodoFooter>
+  </div>
+</template>
+
+<script>
+
+import TodoHeader from './components/TodoHeader.vue'
+import TodoInput from './components/TodoInput.vue'
+import TodoList from './components/TodoList.vue'
+import TodoFooter from './components/TodoFooter.vue'
+
+export default {
+  components: {
+    'TodoHeader': TodoHeader,
+    'TodoInput': TodoInput,
+    'TodoList': TodoList,
+    'TodoFooter': TodoFooter
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Itim&display=swap');
+
+html, body {
+  font-family: 'Itim', cursive;
+}
+body {
+  text-align: center;
+  background-color: #F6F6F6;
+}
+input {
+  border-style: groove;
+  width: 200px;
+}
+button {
+  border-style: groove;
+}
+.shadow {
+  box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
+}
+</style>
---
+++ project01/src/components/TodoFooter.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="inputBox shadow">
+      <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo">
+      <!-- <button v-on:click="addTodo">add</button> -->
+      <span class="addContainer" v-on:click="addTodo">
+        <i class="fas fa-plus addBtn" aria-hidden="true"></i>
+      </span>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      newTodoItem: ''
+    }
+  },
+  methods: {
+    addTodo: function() {
+      if (this.newTodoItem !== '') {
+        var obj = {completed: false, item: this.newTodoItem};
+        //(텍스트,텍스트) 가 아닌 (텍스트, 불린) 을 저장
+        localStorage.setItem(this.newTodoItem, JSON.stringify(obj));
+        this.clearInput();
+      }
+    },
+    clearInput: function() {
+      this.newTodoItem= ''; //비워주기(초기화)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.clearAllContainer {
+  width: 8.5rem;
+  height: 50px;
+  line-height: 50px;
+  background-color: white;
+  border-radius: 5px;
+  margin: 0 auto;
+}
+.clearAllBtn {
+  color: #e20303;
+  display: block;
+}
+</style>
